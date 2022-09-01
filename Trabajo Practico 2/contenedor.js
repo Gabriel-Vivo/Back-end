e2407468ac23c6dbf5ec89476f0d087a092e9d83
Gabriel Vivo <g.vivo@live.com>
2022-09-01 10:53:35 -0300

    trabajo Practico 2 modificado 1/9/2022

--- Trabajo Practico 2/contenedor.js
+++ Trabajo Practico 2/contenedor.js
@@ -1,122 +1,103 @@
-
-const fs = require('fs')
-
-const pathToFile = './mascotas.json' // creamos el archivo json
+let fs = require('fs');
 
 class Contenedor {
 
-   ///////////////////////// //creamos el metodo que nos crea el objeto en este caso el objeto es mascota
+  constructor(archivo) {
+    this.archivo = archivo;
+  }
 
-   
-    crearMascota = async (mascota) =>{
+  crearProducto = async (obj) => {
+    
+    try {
+      if(fs.existsSync(this.archivo)){
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
+        let id = data[data.length - 1].id+1;
+        obj.id = id;
+        data.push(obj);
+        await fs.promises.writeFile(this.archivo,JSON.stringify(data,null,2))
+        return {status: "success", message: "Producto agregado"}
+      }else {
+        obj.id = 1
+        await fs.promises.writeFile(this.archivo, JSON.stringify([obj], null, 2))
+        return {status: "success", message: "Archivo creado"}
+      }
 
-        //validacion de datos ingresados  y
-if(!mascota.nombre || !mascota.animal || !mascota.raza) return { status:"error", message: " Falta completar campos"}
+    } catch (error) {
+      return {status: 'error', message: error.message}
+    }
+  }  
 
-try {
-    if (fs.existsSync(pathToFile)) {
+  busquedaId = async (id) => {
+    try {
+      if(fs.existsSync(this.archivo)){
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
+        let obj = data.find( (item) => item.id === id);
         
-        let data = await fs.promises.readFile(pathToFile, 'utf-8')//leeemos el archivo json creado 
-        let mascotas = JSON.parse(data)//lo convertimos en un array
-        let id = mascotas[mascotas.length-1].id+1 //si el archivo json ya esta creado automatizamnos el id para que se vaya asignando e incrementando en 1
-        mascota.id = id//le pasamos el id creado arriba 
-        mascotas.push(mascota)//pusheamos el nuevo objeto con su nuevo id 
-        await fs.promises.writeFile(pathToFile, JSON.stringify(mascotas, null, 2))// si existe actualiza la informacion creando un nuevo objeto 
-        return {status: "Exitoso", message: "Mascota Creada"}
-    } else {
-        mascota.id = 1 //le pasamos el id inizializado en 1 
-        await fs.promises.writeFile(pathToFile, JSON.stringify([mascota], null, 2))  // creamos el  primer archivo 
-        return {status: "Exitoso", message: "Mascota Creada"}//retorna este obejetos con los siguientes msj  status y message 
+        if(!obj) return {status: 'error', data: `No existe el producto con ID: ${id}`}
+        return {status: 'success', data: obj};
+      }else {
+        return {status:'error', message:`No existe el archivo ${this.archivo}`}
+      }      
+    } catch (error) {
+      return {status:'error', message: error.message}
     }
-} catch(err) {
-    return {status: "error", message: err.message}// en caso de que hubiera un error al crear el archvio muestra el catch con el msj de error 
-}
-}   
+  }
 
+  leerProdcuto = async () => {
+    try {
+      if(fs.existsSync(this.archivo)){
 
-/////////////////////////////// Metodo que lee todos los objetos del archivo JSON
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
 
-leerMascota = async () => {
-    if (fs.existsSync(pathToFile)) { //  corroboramos que exista el archivo 
-        let data = await fs.promises.readFile(pathToFile, 'utf-8') //si existe le pedimos que lea el archivo
-        let mascotas = JSON.parse(data)//parseamos el archivo leido 
-        return {status: "Exitoso", message: mascotas} //lo mostramos como msje en consola 
-    } else {
-        return {status: "error", message: err.message}//si no lee nada retorna msj de error 
+        return {status: 'success', data};
+
+      }else {
+        return {status: 'error', data: `No existe el archivo ${this.archivo}`}
+      }      
+
+    } catch (error) {
+      return {status: 'error', data: error.message}
     }
-}
-////////////////////////////////////////// Metodo que busca por ID
+  }    
 
-busquedaId = async (id) => {
-
-    if (fs.existsSync(pathToFile)) {
-        let data = await fs.promises.readFile(pathToFile, 'utf-8')//llemos nuevamente el archivo creado 
-        let mascotas = JSON.parse(data)//lo parseamos para convertirlo en array 
-        let mascota = mascotas.find(masc => masc.id === id)//recorrermos el array con u  find para buscar el id solicitdo 
-        if (mascota) return {status: "succes", message: mascota}// retornamos el id solicitado 
-       
-    } else {
-        return {status: "error", message: err.message}//msj de error si no existe el archivo 
+  borrarId = async (id) => {
+    try {
+      if(fs.existsSync(this.archivo)){
+        let arch = await fs.promises.readFile(this.archivo, 'utf-8');
+        let data = JSON.parse(arch);
+        
+        let obj = data.find( (item) => item.id === id);        
+        if(!obj) return {status: 'error', data: `No existe el producto con ID: ${id}`}
+  
+        data = data.filter( (item) => item.id !== id);      
+  
+        await fs.promises.writeFile(this.archivo,JSON.stringify(data,null,2))
+        return {status: "success", message: `Producto ID ${id} eliminado con éxito`}
+      }else{
+        return {status:'error', message:`No existe el archivo ${this.archivo}`}
+      }
+    } catch (error) {
+      console.log(`No existe el archivo ${this.archivo}`);
     }
-}
+  }
 
-
-///////////////////////////// Metodo que  Actualizar el archivo 
-actualizarMasc = async (id, actualizarMasc) => {
-
-    if (!id) return {status: "error", message: "Id requerido"}
-    if (fs.existsSync(pathToFile)) {
-        let data = await fs.promises.readFile(pathToFile, 'utf-8')//llemos nuevamente el archivo creado 
-        let mascotas = JSON.parse(data)//lo parseamos para convertirlo en array 
-        let nuevaMascota = mascotas.map(masc => {//hacemos un map del array mascotas ya creado 
-            if (masc.id === id) {//si el id existe actualiza los datos de esa mascota 
-                actualizarMasc.id = id
-                return actualizarMasc
-            } else return masc
-        })
-        await fs.promises.writeFile(pathToFile, JSON.stringify(nuevaMascota, null, 2))
-        return {status: "Exitoso", message: "Mascota Actualizada!"}
-    } else {
-        return {status: "error", message: err.message}
-    }        
-}
-
-
-////////////////////////////////////Metodo que borra el contenido del archivo buscado por ID
-
-borrarMasc = async (id) => {
-    //Vvalidamos el id 
-    if (!id) return {status: "error", message: "Id requerido"}
-    if (fs.existsSync(pathToFile)) {
-        let data = await fs.promises.readFile(pathToFile, 'utf-8')
-        let mascotas = JSON.parse(data)
-        let nuevaMascota = mascotas.filter(masc => masc.id !== id)
-        await fs.promises.writeFile(pathToFile, JSON.stringify(nuevaMascota, null, 2))
-        return {status: "Exitoso", message: "Mascota Eliminada!"}
-    } else {
-        return {status: "error", message: err.message}
+  borrarTodo = async () => {
+    try {
+      if(fs.existsSync(this.archivo)){                
+        await fs.promises.writeFile(this.archivo,JSON.stringify([],null,2))
+        return {status:'succes', message:`Contenido Eliminado`}
+      }else{
+        return {status:'error', message:`No existe el archivo ${this.archivo}`}
+      }      
+    } catch (error) {
+      console.log(`No existe el archivo ${this.archivo}`);
     }
+  }  
 }
 
-
-////////////////////////////////////// Metodo que borra todo el archivo  lo pongo como comentario par que no se ejecute 
-
-
-// borrarTodo = async () => {
-
-//     if (fs.existsSync(pathToFile)) {
-//       await fs.promises.unlinkSync("mascotas.json")
-//     } else {
-//         return {status: "error", message: err.message}
-//     }
-// }
+module.exports = Contenedor;
 
 
-}//cierre del class contenedor 
-
-
-
-
-
-
-    module.exports = Contenedor
--- Trabajo Practico 2/index.js
+++ Trabajo Practico 2/index.js
@@ -1,16 +1,40 @@
-const Contenedor = require("./contenedor.js")
-const contenedor = new Contenedor()
+const Contenedor = require("./contenedor.js");
+let contenedor = new Contenedor("productos.txt");
 
-let mascota = {
-    nombre: "Copito",
-    animal: "Gato",
-    raza: "Siames",
-    edad: 8,
-    
-}
 
-// contenedor.crearMascota(mascota).then(result => console.log(result))  // console que se encarga de crear mascota 
-// contenedor.leerMascota().then(result => console.log(result)) //console que lee el archivo mascota 
-// contenedor.busquedaId(1).then(result => console.log(result)) // console que nos muestra el contenido del archivo buiscado por ID
-// contenedor.actualizarMasc(1,mascota).then(result => console.log(result)) // console que actualiza el archivo Mascotas
-// contenedor.borrarMasc(1).then(result => console.log(result)) // console que nos muestra si fue exitoso el borrar el contenido del archivo 
+contenedor.crearProducto({
+    title: "Globo terraquio",
+    price: 370.6,
+    thumbnail:
+      "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+  })
+  .then((res) => console.log(res));// console que se encarga de crear producto
+
+
+  // contenedor.crearProducto({
+  //   title: "Calculadora",
+  //   price: 250.25,
+  //   thumbnail:
+  //     "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+  // })
+  // .then((res) => console.log(res));// console que se encarga de crear producto
+
+
+
+  // contenedor.crearProducto({
+  //   title: "Regla",
+  //   price: 225.50,
+  //   thumbnail:
+  //     "https://cdn3.iconfinder.com/data/icons/education-209/64/calculator-math-tool-school-256.png",
+  // })
+  // .then((res) => console.log(res));// console que se encarga de crear producto
+
+
+// contenedor.leerProdcuto().then((res) => console.log(res)); //console que lee el archivo producto
+
+//contenedor.busquedaId(8).then((res) => console.log(res)); // console que nos muestra el contenido del archivo buiscado por ID
+
+//contenedor.borrarId(3).then((res) => console.log(res)); //console que borra por ID
+
+//contenedor.borrarTodo().then((res) => console.log(res));//console que nos muestra si fue exitoso el borrar el contenido del archivo
+
